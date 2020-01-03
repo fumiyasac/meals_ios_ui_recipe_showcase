762d8bf89a0616031773115f08b5e4dad35f10ca
Fumiya Sakai <just1factory@gmail.com>
2020-01-03 18:11:24 +0900

    第4章: APIモックサーバーの追加

---
+++ 04_UICollectionViewCompositionalLayoutExample/server_mock/index.js
@@ -0,0 +1,41 @@
+// 参考: json-serverの実装に関する参考資料
+// https://blog.eleven-labs.com/en/json-server
+
+// Mock用のJSONレスポンスサーバーの初期化設定
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+
+// Database構築用のJSONファイル
+const router = jsonServer.router('datasources/db.json');
+
+// 各種設定用
+const middlewares = jsonServer.defaults();
+const rewrite_rules = jsonServer.rewriter({
+    "/api/mock/v1/special/banners" : "/get_banners",
+    "/api/mock/v1/special/pickups" : "/get_pickups",
+    "/api/mock/v1/special/photos?page=:page" : "/get_photos?page=:page",
+    "/api/mock/v1/special/keywords" : "/get_keywords",
+    "/api/mock/v1/special/photo?id=:id"   : "/get_photo_by_id/:id",
+    "/api/mock/v1/special/recommends" : "/get_recommends",
+});
+
+// リクエストのルールを設定する
+server.use(rewrite_rules);
+
+// ミドルウェアを設定する (※コンソール出力するロガーやキャッシュの設定等)
+server.use(middlewares);
+
+// 受信したリクエストにおいてGET送信時のみ許可する
+server.use(function (req, res, next) {
+    if (req.method === 'GET') {
+        next();
+    }
+});
+
+// ルーティングを設定する
+server.use(router);
+
+// サーバをポート3000で起動する
+server.listen(3000, () => {
+    console.log('Chapter02 Mock Server is running...');
+});
